40516fa7941f09152150dedca7ed5842f9d85eda
hijkzzz <janhu9527@gmail.com>
2016-07-14 15:05:30 +0800

    gdt_s.s 修改注释

--- kern/pmm/gdt_s.s
+++ kern/pmm/gdt_s.s
@@ -4,14 +4,14 @@ gdt_flush:
     mov eax, [esp + 4]
     lgdt [eax]
 
-    ;kernel ds 索引号为 2
+    ;内核数据段索引号为 2
     mov ax, 0x10
     mov ds, ax
     mov ss, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    ;kernel cs 索引号为 1
+    ;内核代码段索引号为 1
     jmp 0x08:.flush
 
 .flush
